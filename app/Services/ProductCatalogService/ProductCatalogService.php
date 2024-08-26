dca3705209693a29d9c6842572e0fd3917cccc02
Francisco Bórquez <fborquez@outlook.com>
2024-08-25 22:54:54 -0400

    fixes

--- app/Services/ProductCatalogService/ProductCatalogService.php
+++ app/Services/ProductCatalogService/ProductCatalogService.php
@@ -10,17 +10,6 @@ class ProductCatalogService implements ProductCatalogServiceInterface
 {
     public function getList(): Collection
     {
-        $result = ProductCatalog::with(['category', 'brand', 'type', 'presentation'])
-            ->get()
-            ->map(function ($item) {
-                return [
-                    'id' => $item->id,
-                    'description' => $item->type->description.($item->presentation ? ' '.$item->presentation->description : ''),
-                    'brand' => $item->brand->name,
-                    'category' => $item->category->name,
-                ];
-            });
-
-        return new Collection($result->all());
+        return ProductCatalog::with(['category', 'brand', 'type', 'presentation'])->get();
     }
 }
