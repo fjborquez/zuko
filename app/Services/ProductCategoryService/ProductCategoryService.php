a4d4d46d9e7d6bead9a601f80dd84af487257f8d
Francisco Bórquez <fborquez@outlook.com>
2024-11-24 19:01:59 -0300

    sort product categories

--- app/Services/ProductCategoryService/ProductCategoryService.php
+++ app/Services/ProductCategoryService/ProductCategoryService.php
@@ -5,11 +5,14 @@ namespace App\Services\ProductCategoryService;
 use App\Contracts\Services\ProductCategoryService\ProductCategoryServiceInterface;
 use App\Models\ProductCategory;
 use Illuminate\Database\Eloquent\Collection;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProductCategoryService implements ProductCategoryServiceInterface
 {
     public function getList(): Collection
     {
-        return ProductCategory::all();
+        return QueryBuilder::for(ProductCategory::class)
+            ->allowedSorts('name')
+            ->get();
     }
 }
