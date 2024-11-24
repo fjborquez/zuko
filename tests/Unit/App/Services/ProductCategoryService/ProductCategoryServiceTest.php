06d7a51e260e51de2c24ddb9c98891060b64c7b2
fjborquez <fjborquez@users.noreply.github.com>
2024-11-24 22:28:57 +0000

    PHP Linting (Pint)

--- tests/Unit/App/Services/ProductCategoryService/ProductCategoryServiceTest.php
+++ tests/Unit/App/Services/ProductCategoryService/ProductCategoryServiceTest.php
@@ -3,11 +3,11 @@
 namespace Tests\Unit\App\Services\ProductCategoryService;
 
 use App\Services\ProductCategoryService\ProductCategoryService;
-use function PHPUnit\Framework\assertEquals;
 use Illuminate\Database\Eloquent\Collection;
 use Mockery;
-use Spatie\QueryBuilder\QueryBuilder;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
 
 class ProductCategoryServiceTest extends TestCase
 {
